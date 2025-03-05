f026372e354f8a7f42112ca298cbb7f5f13befb3
denid0ck3r <robbiansyah.deni@gmail.com>
2025-03-05 16:57:35 +0700

    Membuat Variable menggunakan let dan var

---
+++ 04.Variable/variable.js
@@ -0,0 +1,39 @@
+console.log("Oke")
+
+// 01.Variable dengan let 
+let nama = "Ucup Marucup";
+console.log(nama)
+
+//ubah variable nama, tidak perlu pakai let lagi.
+nama = "Otong Surotong"
+console.log(nama)
+
+// 02.Variable dengan Var (global&local)
+var namaDepan = "Kucup"
+console.log(namaDepan)
+namaDepan = "Kembang"
+console.log(namaDepan)
+
+// Kelakuan dari Let
+let namaBelakang = "Surucup"
+{
+    let namaBelakang = "Surotong"
+    console.log(namaBelakang)
+}
+console.log(namaBelakang)
+
+// Kelakuan dari Var
+var namaTengah = "Susilo Bambang Yudhoyono"
+{
+    var namaTengah = "Joko Widodo"
+    console.log(namaTengah)
+}
+console.log(namaTengah)
+
+// kasus khusus tanpa keyword akan jadi var 
+gorengan ="Bala-bala"
+{
+    gorengan = "tempe";
+
+}
+console.log(gorengan)
